cfca5c7ef86ddab5a47e8a007d60ee1410aec243
AraafRoyall ❂ <swordigoar@gmail.com>
2024-08-15 16:44:10 +0530

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,4 +6,4 @@ set_perm_recursive "$MODPATH/system/bin" root root 0777 0755
 ui_print "[*] All permissions set..."
 ui_print "[*] Done..."
 
-ui_print "[*] This Module made by AraafRoyall"
+ui_print "[*] This Module is modified by AraafRoyall"
